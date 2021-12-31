15934ac820cf81ea690a1ad851750d4470b97773
matt6852 <prorokwow@mail.ru>
2021-12-31 19:57:05 +0300

    test1

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -4,17 +4,8 @@ import ImgBg from "./Imgbagraund";
 import ProfileInfo from "./ProfileInfo";
 
 const ProfilePage = () => {
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
-    console.log("test!")
+    console.log("test! test test")
+
     return (
         <>
             <ProfileInfo/>
