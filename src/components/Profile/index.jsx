1a51914da34ce7e78324013026b5d29a9f99901a
matt6852 <prorokwow@mail.ru>
2021-12-31 19:56:22 +0300

    test1

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -7,6 +7,10 @@ const ProfilePage = () => {
     console.log("test!")
     console.log("test!")
     console.log("test!")
+    console.log("test!")
+    console.log("test!")
+    console.log("test!")
+    console.log("test!")
     return (
         <>
             <ProfileInfo/>
