1ea09c531ffdbc2e72fded590dd7d73a8b5890d9
matt6852 <prorokwow@mail.ru>
2021-12-31 11:13:43 +0300

    added dialog page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import NavBar from "./components/Navbar";
 import ImgBg from "./components/Imgbagraund";
 import ProfilePage from "./components/Profile";
 import PostsPage from "./components/PostsPage";
+import Dialogs from "./components/Dialogs";
 
 function App() {
-  return (
-    <div className="appWrapper">
-      <Header />
-      <div className="navWrapper">
-        <NavBar />
-        <div className="mainContent">
-          <ImgBg />
-          <ProfilePage />
-          {/* <PostsPage /> */}
+    return (
+        <div className="appWrapper">
+            <Header/>
+            <div className="navWrapper">
+                <NavBar/>
+                <div className="mainContent">
+                    <ImgBg/>
+                    {/*<ProfilePage />*/}
+                    <Dialogs/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Dialogs/index.jsx
@@ -0,0 +1,10 @@
+import style from "./style.module.css"
+
+
+const Dialogs = () => {
+    return (
+        <h1 className={style.header}> Dialogs</h1>
+
+    )
+}
+export default Dialogs
